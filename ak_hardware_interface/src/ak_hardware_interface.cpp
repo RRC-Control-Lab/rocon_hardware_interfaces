cbce073d03ab750a36d49ed1808ff985c5c63248
Soham Patil <sohampatil45939@gmail.com>
2024-06-21 19:39:14 +0530

    slight logic change

--- ak_hardware_interface/src/ak_hardware_interface.cpp
+++ ak_hardware_interface/src/ak_hardware_interface.cpp
@@ -352,12 +352,14 @@ hardware_interface::CallbackReturn AKHardwareInterface::on_activate(
       RCLCPP_INFO(
         rclcpp::get_logger("AKHardwareInterface"), "'%s': Got Endstop",info_.joints[i].name.c_str());
       motor_[i].homing_offset = motor_[i].raw_position_rad;
-      while(!send_torque(&motor_[i],0.0));
       RCLCPP_INFO(
         rclcpp::get_logger("AKHardwareInterface"), "Homing Offset for '%s' detected as %lf",info_.joints[i].name.c_str(),motor_[i].homing_offset);
       RCLCPP_INFO(
         rclcpp::get_logger("AKHardwareInterface"), "'%s': Waiting for Speed Down",info_.joints[i].name.c_str());
-      while(fabs(motor_[i].raw_velocity_rad_s)>1);
+      while(fabs(motor_[i].raw_velocity_rad_s)>1)
+      {
+        while(!send_torque(&motor_[i],0.0));
+      }
       RCLCPP_INFO(
         rclcpp::get_logger("AKHardwareInterface"), "'%s': Speed Down",info_.joints[i].name.c_str());
       motor_[i].homing_done = true;
